b5c835a7673e81c6d31da5fd9c70851f81ce7d46
tuxp1 <tuxp1@lenovo.com>
2019-06-06 01:21:11 +0800

    [Rx][FileManager]Fix isUsb or isSd bug

--- app/src/main/java/com/tustar/filemanager/model/VideoItem.kt
+++ app/src/main/java/com/tustar/filemanager/model/VideoItem.kt
@@ -9,29 +9,29 @@ class VideoItem : MediaItem() {
     companion object {
         fun toList(cursor: Cursor): List<VideoItem> {
             val items = mutableListOf<VideoItem>()
-            cursor.moveToFirst()
-            do {
-                val item = VideoItem()
-                //
-                val id = cursor.getLong(cursor.getColumnIndex(
-                        MediaStore.Video.VideoColumns._ID))
-                val displayName = cursor.getString(cursor.getColumnIndex(
-                        MediaStore.Video.VideoColumns.DISPLAY_NAME))
-                val mimeType = cursor.getString(cursor.getColumnIndex(
-                        MediaStore.Video.VideoColumns.MIME_TYPE))
-                val dateModified = cursor.getLong(cursor.getColumnIndex(
-                        MediaStore.Video.VideoColumns.DATE_MODIFIED))
-                val size = cursor.getLong(cursor.getColumnIndex(
-                        MediaStore.Video.VideoColumns.SIZE))
-                //
-                item.name = displayName
-                item.type = mimeType
-                item.lastModified = dateModified
-                item.length = size
-                item.uri = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id)
-                items += item
-            } while (cursor.moveToNext())
-
+            if (cursor.moveToFirst() && cursor.count > 0) {
+                do {
+                    val item = VideoItem()
+                    //
+                    val id = cursor.getLong(cursor.getColumnIndex(
+                            MediaStore.Video.VideoColumns._ID))
+                    val displayName = cursor.getString(cursor.getColumnIndex(
+                            MediaStore.Video.VideoColumns.DISPLAY_NAME))
+                    val mimeType = cursor.getString(cursor.getColumnIndex(
+                            MediaStore.Video.VideoColumns.MIME_TYPE))
+                    val dateModified = cursor.getLong(cursor.getColumnIndex(
+                            MediaStore.Video.VideoColumns.DATE_MODIFIED))
+                    val size = cursor.getLong(cursor.getColumnIndex(
+                            MediaStore.Video.VideoColumns.SIZE))
+                    //
+                    item.name = displayName
+                    item.type = mimeType
+                    item.lastModified = dateModified
+                    item.length = size
+                    item.uri = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id)
+                    items += item
+                } while (cursor.moveToNext())
+            }
             return items
         }
     }
--- app/src/main/java/com/tustar/filemanager/ui/MainActivity.kt
+++ app/src/main/java/com/tustar/filemanager/ui/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.tustar.filemanager.ui
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.tbruyelle.rxpermissions2.RxPermissions
 import com.tustar.rxjava.R
+import com.tustar.rxjava.util.Logger
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setTitle(R.string.title_activity_files)
+
+        //
+        checkPermissionRequest(this)
+    }
+
+    private fun checkPermissionRequest(activity: FragmentActivity) {
+        val permissions = RxPermissions(activity)
+        permissions.setLogging(true)
+
+        permissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe { granted ->
+                    Logger.d("granted = $granted")
+                    if (granted) { // Always true pre-M
+                        // I can control the camera now
+                    } else {
+                        // Oups permission denied
+                    }
+                }
+
     }
 }
